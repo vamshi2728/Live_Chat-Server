5e098458e628283f88482283d6856b229f269de4
vamshi2728 <kondavamshi444@gmail.com>
2026-09-22 18:37:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "https://vamshi2728.github.io",
+    methods: ["GET", "POST"]
+  }
+});
 
 const PORT = process.env.PORT || 3000;
 
